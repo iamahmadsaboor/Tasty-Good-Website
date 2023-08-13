3016378d277224535bfa05f3d12c63dcb21da50a
iamahmadsaboor <ahmadsaboor020@gmail.com>
2023-08-13 14:22:17 +0500

    Logo Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Meals from "./components/Meals";
 import Friedwonton from "./components/Friedwonton";
@@ -8,9 +8,20 @@ import Membership from "./components/Membership";
 import Location from "./components/Location";
 import Footer from "./components/Footer";
 function App() {
+  // {
+  //   alert("The Website is Under Construction!!! Feedback Will Be Appreciated");
+  // }
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
+      {/* <head>
+        <title>My Site</title>
+        <link
+          rel="shortcut icon"
+          href="../public/favicon.ico"
+          type="image/x-icon"
+        />
+      </head> */}
       <Main />
       <Meals />
       <Friedwonton />
