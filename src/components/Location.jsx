51803094439f37dbc6a16d9521d937198817fa6b
iamahmadsaboor <ahmadsaboor020@gmail.com>
2023-08-22 00:27:13 +0500

    Location Section updated

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -4,10 +4,10 @@ const Location = () => {
   return (
     <div className="flex items-center justify-around">
       <div className="w-[40%] pt-6  ">
-        <h4>Our Resturant</h4>
-        <h1>Locations</h1>
+        <h4 className="text-3xl text-red-600 ">Our Resturant</h4>
+        <h1 className="text-2xl text-black ">Locations</h1>
         <p>We are available in JAKARTA, BANDUNG, SURABAYA, and BALI.</p>
-        <button className="px-4 py-2 font-semibold text-blue-700 bg-transparent border border-blue-500 rounded-full hover:bg-blue-500 hover:text-white hover:border-transparent ">
+        <button className="px-4 py-2 font-semibold text-blue-700 bg-transparent border border-blue-500 rounded-full hover:bg-orange-500 hover:text-white hover:border-transparent ">
           Learn More
         </button>
       </div>
